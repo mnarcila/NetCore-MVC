696fb56f584d14b195d2343aed74c16be0ef9af0
Mario <mnarcila@gmail.com>
2019-10-25 00:13:04 -0500

    commit
    commit final con pruebas y ajustes menores

--- Pages/login/Login.cs
+++ Pages/login/Login.cs
@@ -7,7 +7,7 @@ using netcore.model;
 namespace netcore.Pages.login {
     public class Login : PageModel {
         public const string SessionKeyName = "_Name";
-        
+
         public IActionResult OnGet () {
             return Page ();
         }
@@ -15,25 +15,35 @@ namespace netcore.Pages.login {
         [BindProperty]
         public Usuario usuario { get; set; }
         public async Task<IActionResult> OnPostAsync () {
-            if (!ModelState.IsValid) {
-                Console.WriteLine ("Entro1");
-                return Page ();
-            } else {
-                Console.WriteLine ("Entro2");
+            try {
 
-                if (usuario.Nombre.Equals ("admin") && usuario.Clave.Equals ("admin")) {
-
-                    if (string.IsNullOrEmpty (HttpContext.Session.GetString (SessionKeyName))) {
-                        HttpContext.Session.SetString (SessionKeyName, usuario.Nombre);
-
-                    }
-                    return RedirectToPage ("../home/Home");
-                } else {
-                    usuario.Mensaje = "Usuario o clave incorrecto";
+                if (!ModelState.IsValid) {
+                    Console.WriteLine ("Entro1");
                     return Page ();
+                } else {
+                    Console.WriteLine ("Entro2");
+
+                    //control de error en pagina 
+
+                    if (usuario.Nombre.Equals ("admin") && usuario.Clave.Equals ("admin")) {
+
+                        if (string.IsNullOrEmpty (HttpContext.Session.GetString (SessionKeyName))) {
+                            HttpContext.Session.SetString (SessionKeyName, usuario.Nombre);
+
+                        }
+                        return RedirectToPage ("../home/Home");
+                    } else if (usuario.Nombre.Equals ("error")) {
+                        return RedirectToPage ("../Error");
+                    } else {
+                        usuario.Mensaje = "Usuario o clave incorrecto";
+                        return Page ();
+                    }
                 }
-            } 
+            } catch {
+                return RedirectToPage ("../Error");
+            }
         }
+
     }
 
 }
--- Startup.cs
+++ Startup.cs
@@ -45,15 +45,12 @@
                  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                  app.UseHsts ();
              }
-
              app.UseHttpsRedirection ();
              app.UseStaticFiles ();
-
+             app.UseSession ();
              app.UseRouting ();
-
              app.UseMvc ();
              app.UseAuthorization ();
-             app.UseSession ();
              app.UseEndpoints (endpoints => {
                  endpoints.MapRazorPages ();
              });
